363b34dd62349d3e717701f4dbf396d770685462
NorthZeroD <northzero_9376@outlook.com>
2026-02-06 17:10:15 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-clang tetris.c -o tetris -g -ggdb3 -O0 -std=c23 -Wall -Wextra -I./include -L./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+#!/usr/bin/bash
+
+mkdir -p build
+clang src/tetris.c -o build/tetris -g -ggdb3 -O0 -std=c23 -Wall -Wextra -I./include -L./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
