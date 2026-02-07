202e4f1a4e52bd9cc6140a2f0824071337ab53ea
NorthZeroD <northzero_9376@outlook.com>
2026-02-08 00:05:57 +0800

    use raylib official cmake template

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-mkdir -p build
-clang src/tetris.c -o build/tetris -g -ggdb3 -O0 -std=c23 -Wall -Wextra -I./include -L./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-# clang src/tetris.c -o build/tetris -O3 -std=c23 -Wall -Wextra -I./include -L./lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-
