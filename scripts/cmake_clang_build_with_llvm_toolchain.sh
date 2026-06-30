18a6c312eaf4c5e2a96d631bc44e080411e17e86
NorthZeroD <northzero_9376@outlook.com>
2026-07-01 04:08:53 +0800

    add useful scripts

--- scripts/cmake_clang_build_with_llvm_toolchain.sh
+++ scripts/cmake_clang_build_with_llvm_toolchain.sh
@@ -5,7 +5,9 @@ cmake -S . -B build -G Ninja \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+    -DCMAKE_LINKER_TYPE=LLD
 
 # build
 cmake --build build
--- scripts/cmake_gcc_build.sh
+++ scripts/cmake_gcc_build.sh
@@ -4,8 +4,8 @@
 cmake -S . -B build -G Ninja \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++
 
 # build
 cmake --build build
